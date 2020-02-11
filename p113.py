2d90eac1d8780a02ca0f534f088a1f86dfc3f632
HermonMulat <hermonmulat@gmail.com>
2020-02-11 00:35:36 -0500

    Add solution to p113

--- p113.py
+++ p113.py
@@ -1,32 +1,36 @@
 import sys
-def reversePrefix(a):
-    sub = 0
-    t = sum(a)
-    ans = []
-    for i in a:
-        ans.append(t-sub)
-        sub += i
-    return ans
-def prefix(a):
-    ans =[a[0]]
 
-    for i in range(1,len(a)):
-        ans.append(a[i]+ans[i-1])
-    return ans
+def increasing(length):
+    count = [1 for i in xrange(10)]
+    count[0] = 0
+    total = 0
+    for i in xrange(1, length):
+        total += sum(count)
+        nxt = [0 for i in xrange(10)]
+        for ending in xrange(10):
+            for extension in xrange(ending,10):
+                nxt[extension] += count[ending]
+        count, nxt = nxt, count
+    total += sum(count)
+    return total
 
-
-def inc(n):
-    table = [[1,1,1,1,1,1,1,1,1] for i in range(n)]
-
-    for i in range(1,n):
-        table[i] = prefix(table[i-1])
-
-    return table
+def decreasing(length):
+    count = [1 for i in xrange(10)]
+    count[0] = 0
+    total = 0
+    for i in xrange(1, length):
+        total += sum(count)
+        nxt = [0 for i in xrange(10)]
+        for ending in xrange(10):
+            for extension in xrange(0,ending+1):
+                nxt[extension] += count[ending]
+        count, nxt = nxt, count
+    total += sum(count)
+    return total
 
 def main():
-    a = dec(int(sys.argv[1]))
-    for i in a:
-        print i, " ==> ",sum(i)
+    length = int(sys.argv[1])
+    print increasing(length)+decreasing(length)-(9*length)
 
 if __name__ == '__main__':
     main()
