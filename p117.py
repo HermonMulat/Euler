51d10bb74f3db1d82d142ce4c39f16769eb58a46
Hermon Mulat <hmmulat@amazon.com>
2020-02-12 17:26:21 -0800

    Add solution to p117

---
+++ p117.py
@@ -0,0 +1,19 @@
+import sys
+
+def block_combinations(n,m):
+  table = [1 for i in xrange(0,n+1)]
+  for N in xrange(m, n+1):
+    for min_len in xrange(m, min(5,N+1)):
+      for pos in xrange(N-min_len+1):
+        new_n = N-min_len-pos
+        if new_n < m:
+          table[N] += 1
+        else:
+          table[N] += table[new_n]
+  return table[-1]
+
+def main():
+  print block_combinations(int(sys.argv[1]),2)
+
+if __name__ == '__main__':
+  main()
